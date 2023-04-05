a17c7baaca1f1e34b42e427de5186ccb035039fe
Maria Jenkins <macjenkin@gmail.com>
2023-04-05 17:41:20 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array) {
+    let win = array.find(game => game.result === "W");
+    if (win === undefined) {
+        return undefined
+    } else {
+        return win.year
+    }
+}
